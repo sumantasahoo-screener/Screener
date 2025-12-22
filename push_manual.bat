b81423de7a4495ed7ae0f2c4dd00716d006d90ee
sumantasahoo <sumantasahoo@gmail.com>
2025-12-22 19:37:05 +0530

    auto update

--- push_manual.bat
+++ push_manual.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d D:\ScreenerGIT\Screener
 
-git add manual_detail.html manual_overview.html
+git add selection.html manual_overview.html
 git commit -m "add manual html files"
 git push
 
